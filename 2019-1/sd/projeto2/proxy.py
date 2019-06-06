302617834a5bec37d0e9cb98588a917664e774b8
Bruna Zamith <41006576+bzamith@users.noreply.github.com>
2019-06-06 10:25:18 -0300

    Remove unnecessary imports

--- 2019-1/sd/projeto2/proxy.py
+++ 2019-1/sd/projeto2/proxy.py
@@ -2,13 +2,7 @@
 # This shows how to capture data using a pub-sub proxy
 # https://github.com/booksbyus/zguide/blob/master/examples/Python/espresso.py
 
-import time
-
-from threading import Thread
 import zmq
-from zmq.devices import monitored_queue
-import binascii
-import os
 
 # main thread
 # The main task starts the subscriber and publisher, and then sets
