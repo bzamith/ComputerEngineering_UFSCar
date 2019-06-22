6c1c71d872bba1fda52893984f6473da097dd26d
Bruna Zamith <41006576+bzamith@users.noreply.github.com>
2019-06-21 23:16:49 -0300

    aprendi sincronizacao yeay

---
+++ 2019-1/so2/estudosopenmp/integralpi-5.c
@@ -0,0 +1,29 @@
+// integral de 0 a 1 de 4 / (1+x²), tem que dar pi
+// integracao numerica
+
+
+// AGORA USEI MECANISMO DE SINCRONIZAÇÃO
+
+#include <omp.h>
+#include <stdio.h>
+
+static int num_steps = 2;
+double step;
+int main(int *argc, char *argv[]){
+	double pi, sum = 0.0;
+	step = 1.0/(double) num_steps;
+
+	#pragma omp parallel num_threads(num_steps)
+	{
+		double x = 0.0;
+		double i = (double) omp_get_thread_num();
+		x = (i+0.5)*step;
+		#pragma omp critical
+		{ 
+			sum = sum + 4.0/(1.0+x*x);
+		}
+	}
+
+	pi = step*sum;
+	printf("%f\n",pi);
+}
