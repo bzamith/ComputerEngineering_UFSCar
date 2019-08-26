e91565b9603392008b23b92d17d78317729fe863
Matheus Vrech <whoismath@gmail.com>
2019-08-26 11:17:26 -0300

    algumas modificações, ainda não está completo

--- 2019-2/siai/ga.py
+++ 2019-2/siai/ga.py
@@ -1,10 +1,31 @@
 import random
+import collections
+import operator
+
 class Cromossomo:
 	def __init__(self, kp, ki, kd):
 		self.kp = kp
 		self.ki = ki
 		self.kd = kd
-		self.taxa_erro
+		self.taxa_erro = 0
+	def calculaTaxaErro(self):
+		return 10
+	def printaCromossomo(self):
+		print("Kp = "+str(self.kp)+"\n"
+			"Ki = "+str(self.ki)+"\n"
+			"Kd = "+str(self.kd)+"\n"
+			"Erro = "+str(self.taxa_erro)+"\n")
+
+def calculaRangeKp():
+	return 1,9
+
+def calculaRangeKi():
+	return 7,20.8
+
+def calculaRangeKd():
+	return 10,30.5
+
+
 
 def populacaoInicial(qntd):
 	populacao = list()	
@@ -14,18 +35,21 @@ def populacaoInicial(qntd):
 	for i in range(0, qntd):
 		populacao.append(Cromossomo(
 			random.uniform(pmin,pmax),
-			random.uniform(imin,imax).
-			ranfom.uniform(dmin,dmax)))
+			random.uniform(imin,imax),
+			random.uniform(dmin,dmax)))
 	return populacao
 
-def calculaErro(cromossomo):
-	erro = calculaTaxaErro(cromossomo)
-	cromossomo.taxa_erro = erro
-
-def selecao(populacao, qtnd):
-	erros = list()	
+def selecao(populacao, qntd):
+	erros = {}	
 	for i in range(0, len(populacao)):
-		calculaErro(populacao[i])
-		erros.append(populacao[i].taxa_erro)
-	erros.sort()
-	return erros[0:qntd]
+		erros[i]=populacao[i].calculaTaxaErro()
+		print(erros[i])
+	#erros.sort()
+	sorted_erros = sorted(erros.items(), key=lambda x: x[1])
+	sorted_dict = collections.OrderedDict(sorted_erros)
+	return 
+
+if __name__ == "__main__":
+	teste = selecao(populacaoInicial(10),10)
+	for i in range(0,10):
+		teste[i].printaCromossomo()
