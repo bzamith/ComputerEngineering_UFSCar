d6fc66a189f5c04fd70f788055342dbef94cabd4
Bruna Zamith <41006576+bzamith@users.noreply.github.com>
2019-09-02 08:11:03 -0300

    Update ga.py

--- 2019-2/siai/ga.py
+++ 2019-2/siai/ga.py
@@ -1,6 +1,8 @@
 import random
-import collections
 import operator
+
+POP_TAM = 10
+POP_SEL = 5
 
 class Cromossomo:
 	def __init__(self, kp, ki, kd):
@@ -9,8 +11,8 @@ class Cromossomo:
 		self.kd = kd
 		self.taxa_erro = 0
 	def calculaTaxaErro(self):
-		self.taxa_erro = 10
-		return 10
+		self.taxa_erro = random.uniform(0,1)
+		return self.taxa_erro
 	def printaCromossomo(self):
 		print("Kp = "+str(self.kp)+"\n"
 			"Ki = "+str(self.ki)+"\n"
@@ -26,26 +28,26 @@ def calculaRangeKi():
 def calculaRangeKd():
 	return 10,30.5
 
-def populacaoInicial(qntd):
+def populacaoInicial():
 	populacao = list()	
 	pmin, pmax = calculaRangeKp()
 	imin, imax = calculaRangeKi()
 	dmin, dmax = calculaRangeKd()		
-	for i in range(0, qntd):
+	for i in range(0, POP_TAM):
 		populacao.append(Cromossomo(
 			random.uniform(pmin,pmax),
 			random.uniform(imin,imax),
 			random.uniform(dmin,dmax)))
 	return populacao
 
-def selecao(populacao, qntd):
+def selecao(populacao):
 	erros = {}	
 	for i in range(0, len(populacao)):
 		populacao[i].calculaTaxaErro()
 	sorted_erros = sorted(populacao, key=operator.attrgetter('taxa_erro'))
-	return sorted_erros
+	return sorted_erros[0:POP_SEL]
 
 if __name__ == "__main__":
-	teste = selecao(populacaoInicial(10),10)
-	for i in range(0,10):
+	teste = selecao(populacaoInicial())
+	for i in range(0,POP_SEL):
 		teste[i].printaCromossomo()
