aaa9d5ff65eced8ce0c4c6a9d9ca9d52f50fc562
Bruna Zamith <41006576+bzamith@users.noreply.github.com>
2019-09-02 08:39:08 -0300

    faz cruzamento

--- 2019-2/siai/ga.py
+++ 2019-2/siai/ga.py
@@ -1,23 +1,36 @@
+from copy import deepcopy
 import random
 import operator
 
-POP_TAM = 10
-POP_SEL = 5
+POP_TAM = 12
+POP_SEL = POP_TAM/2
 
 class Cromossomo:
-	def __init__(self, kp, ki, kd):
-		self.kp = kp
-		self.ki = ki
-		self.kd = kd
-		self.taxa_erro = 0
-	def calculaTaxaErro(self):
-		self.taxa_erro = random.uniform(0,1)
-		return self.taxa_erro
-	def printaCromossomo(self):
-		print("Kp = "+str(self.kp)+"\n"
-			"Ki = "+str(self.ki)+"\n"
-			"Kd = "+str(self.kd)+"\n"
-			"Erro = "+str(self.taxa_erro)+"\n")
+    def __init__(self, kp, ki, kd):
+	self.kp = kp
+	self.ki = ki
+	self.kd = kd
+        self.taxa_erro = 0
+    def getKp(self):
+        return self.kp
+    def setKp(self,kp):
+        self.kp = kp
+    def getKi(self):
+        return self.ki
+    def setKi(self,ki):
+        self.ki = ki
+    def getKd(self):
+        return self.kd
+    def setKd(self,kd):
+        self.kd = kd
+    def calculaTaxaErro(self):
+        self.taxa_erro = random.uniform(0,1)
+        return self.taxa_erro
+    def printaCromossomo(self):
+	print("Kp = "+str(self.kp)+"\n"
+	    "Ki = "+str(self.ki)+"\n"
+	    "Kd = "+str(self.kd)+"\n"
+	    "Erro = "+str(self.taxa_erro)+"\n")
 
 def calculaRangeKp():
 	return 1,9
@@ -47,7 +60,19 @@ def selecao(populacao):
 	sorted_erros = sorted(populacao, key=operator.attrgetter('taxa_erro'))
 	return sorted_erros[0:POP_SEL]
 
+def cruzamento(populacao):
+    filhos = deepcopy(populacao)
+    for i in range(0, len(filhos), 2):
+        temp = filhos[i].getKp() 
+        filhos[i].setKp(filhos[i+1].getKp())
+        filhos[i+1].setKp(temp)
+    return filhos
+
 if __name__ == "__main__":
-	teste = selecao(populacaoInicial())
-	for i in range(0,POP_SEL):
-		teste[i].printaCromossomo()
+	pop_selecionada = selecao(populacaoInicial())
+	for i in range(0,len(pop_selecionada)):
+		pop_selecionada[i].printaCromossomo()
+	print("---------------------------------------------------------")
+	nova_pop = pop_selecionada+cruzamento(pop_selecionada)
+	for i in range(0,len(nova_pop)):
+		nova_pop[i].printaCromossomo()
