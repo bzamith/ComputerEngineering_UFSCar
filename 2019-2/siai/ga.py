ce60e9329415252d9061b7ca5c87f004ce036b88
Bruna Zamith <41006576+bzamith@users.noreply.github.com>
2019-09-02 09:20:54 -0300

    comentado

--- 2019-2/siai/ga.py
+++ 2019-2/siai/ga.py
@@ -2,15 +2,18 @@ from copy import deepcopy
 import random
 import operator
 
+# Variaveis globais, POP_TAM deve ser um numero multiplo de 4
 POP_TAM = 12
 POP_SEL = POP_TAM/2
+MAX_GERACAO = 5
 
+# Classe para Cromossomo do AG 
 class Cromossomo:
     def __init__(self, kp, ki, kd):
-	self.kp = kp
-	self.ki = ki
-	self.kd = kd
-        self.taxa_erro = 0
+    	self.kp = kp
+    	self.ki = ki
+    	self.kd = kd
+    	self.taxa_erro = 0
     def getKp(self):
         return self.kp
     def setKp(self,kp):
@@ -23,6 +26,8 @@ class Cromossomo:
         return self.kd
     def setKd(self,kd):
         self.kd = kd
+    def getTaxaErro(self):
+        return self.taxa_erro
     def calculaTaxaErro(self):
         self.taxa_erro = random.uniform(0,1)
         return self.taxa_erro
@@ -32,15 +37,19 @@ class Cromossomo:
 	    "Kd = "+str(self.kd)+"\n"
 	    "Erro = "+str(self.taxa_erro)+"\n")
 
+# Funcao para simular o range de Kp
 def calculaRangeKp():
 	return 1,9
 
+# Funcao para simular o range de Ki
 def calculaRangeKi():
 	return 7,20.8
 
+# Funcao para simular o range de Kd
 def calculaRangeKd():
 	return 10,30.5
 
+# Cria a populacao inicial baseada nos ranges
 def populacaoInicial():
 	populacao = list()	
 	pmin, pmax = calculaRangeKp()
@@ -53,6 +62,7 @@ def populacaoInicial():
 			random.uniform(dmin,dmax)))
 	return populacao
 
+# Faz a selecao da populacao
 def selecao(populacao):
 	erros = {}	
 	for i in range(0, len(populacao)):
@@ -60,6 +70,7 @@ def selecao(populacao):
 	sorted_erros = sorted(populacao, key=operator.attrgetter('taxa_erro'))
 	return sorted_erros[0:POP_SEL]
 
+# Faz o cruzamento, altera apenas o Kp
 def cruzamento(populacao):
     filhos = deepcopy(populacao)
     for i in range(0, len(filhos), 2):
@@ -68,11 +79,20 @@ def cruzamento(populacao):
         filhos[i+1].setKp(temp)
     return filhos
 
+# Funcao principal
 if __name__ == "__main__":
-	pop_selecionada = selecao(populacaoInicial())
-	for i in range(0,len(pop_selecionada)):
-		pop_selecionada[i].printaCromossomo()
-	print("---------------------------------------------------------")
-	nova_pop = pop_selecionada+cruzamento(pop_selecionada)
-	for i in range(0,len(nova_pop)):
+    pop_inicial = populacaoInicial()
+    geracao = 1
+    
+    while(geracao <= MAX_GERACAO):
+        pop_selecionada = selecao(pop_inicial)
+        menor = pop_selecionada[0]
+        nova_pop = pop_selecionada+cruzamento(pop_selecionada)
+        print("Geracao: "+str(geracao))
+        for i in range(0,len(nova_pop)):
 		nova_pop[i].printaCromossomo()
+	print("-----------------------------------------------------")
+    	pop_inicial = deepcopy(nova_pop)
+	geracao=geracao+1
+	
+    menor.printaCromossomo()
